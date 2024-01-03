13315b70bb184625eeff32ca0adea90df809cbf3
premvarma2002 <vakilverma1982@gmail.com>
2024-01-03 23:41:56 +0530

    Navbar and Dark mode append

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,9 @@ export const metadata: Metadata = {
         media: "(prefers-color-scheme: dark)",
         url: "/logo-dark.svg",
         href: "/logo-dark.svg",
-      },
-    ],
-  },
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
@@ -29,8 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider 
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        storageKey="jotion-theme"
+        >
+        {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
