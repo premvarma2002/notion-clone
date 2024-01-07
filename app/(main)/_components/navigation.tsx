902a825da631e675013bbc47a3e6474c5779c3d7
premvarma2002 <vakilverma1982@gmail.com>
2024-01-08 00:10:03 +0530

    Search and Settings option added to sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,38 +1,41 @@
 "use client";
 
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { ElementRef , useEffect, useRef, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { api } from "@/convex/_generated/api";
 
 import { cn } from "@/lib/utils";
 import { UserItem } from "./user-item";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
+import { Item } from "./item";
+import { toast } from "sonner";
 
 export const Navigation = () => {
   const pathname = usePathname();
-  const isMobile = useMediaQuery("(max-width: 768px)")
+  const isMobile = useMediaQuery("(max-width: 768px)");
   const documents = useQuery(api.documents.get);
+  const create =  useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
   const [isResetting, setIsResetting] = useState(false);
-  const[isCollapsed, setIsCollapsed] = useState(isMobile);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
   useEffect(() => {
-   if (isMobile) {
-    collapse();
-   } else {
-    resetWidth();
-   }
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
   }, [isMobile]);
 
   useEffect(() => {
-if (isMobile) {
-collapse();
-}
+    if (isMobile) {
+      collapse();
+    }
   }, [pathname, isMobile]);
 
   const handleMouseDown = (
@@ -48,7 +51,7 @@ collapse();
 
   const handleMouseMove = (event: MouseEvent) => {
     if (!isResizingRef.current) return;
-    let newWidth = event.clientX; 
+    let newWidth = event.clientX;
 
     if (newWidth < 240) newWidth = 240;
     if (newWidth > 480) newWidth = 480;
@@ -56,7 +59,10 @@ collapse();
     if (sidebarRef.current && navbarRef.current) {
       sidebarRef.current.style.width = `${newWidth}px`;
       navbarRef.current.style.setProperty("left", `${newWidth}px`);
-      navbarRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% - ${newWidth}px)`
+      );
     }
   };
 
@@ -67,7 +73,7 @@ collapse();
   };
 
   const resetWidth = () => {
-     if (sidebarRef.current && navbarRef.current) {
+    if (sidebarRef.current && navbarRef.current) {
       setIsCollapsed(false);
       setIsResetting(true);
 
@@ -76,13 +82,10 @@ collapse();
         "width",
         isMobile ? "0" : "calc(100%-240px)"
       );
-      navbarRef.current.style.setProperty(
-        "left",
-        isMobile ? "100%" : "240px"
-      )
-      setTimeout(() => setIsResetting(false), 300)
-     }
-  }
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
 
   const collapse = () => {
     if (sidebarRef.current && navbarRef.current) {
@@ -90,53 +93,84 @@ collapse();
       setIsResetting(true);
 
       sidebarRef.current.style.width = "0";
-      navbarRef.current.style.setProperty("width" , "100%");
+      navbarRef.current.style.setProperty("width", "100%");
       navbarRef.current.style.setProperty("left", "0");
       setTimeout(() => setIsResetting(false), 300);
     }
-  }
+  };
+
+ const handleCreate = () => {
+ const promise = create({ title: "Untitled"});
+
+ toast.promise(promise , {
+  loading: "Creating a new note...",
+  success: "New note created!",
+  error : "Failed to create a new note"
+ })
+ }
 
   return (
     <>
-      <aside ref={sidebarRef} className={cn(
-        "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
-        isResetting && "transition-all ease-in-out duration-300",
-        isMobile && "w-0"
-        )}>
-        <div
-        onClick={collapse}
-        role="button"
+      <aside
+        ref={sidebarRef}
         className={cn(
-          "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bbg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
-          isMobile && "opacity-100"
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
+        )}
+      >
+        <div
+          onClick={collapse}
+          role="button"
+          className={cn(
+            "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bbg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100"
           )}
-          >
-          <ChevronsLeft className="h-6 w-6"/>
+        >
+          <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-<UserItem />
+          <UserItem />
+          <Item 
+          label="Search"
+          icon={Search}
+          isSearch
+          onClick={() => {}}
+          />
+          <Item 
+          label="Settings"
+          icon={Settings}
+          onClick={() => {}}
+          />
+          <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
           {documents?.map((document) => (
-           <p key={document._id}>{document.title}</p>
+            <p key={document._id}>{document.title}</p>
           ))}
         </div>
         <div
-        onMouseDown= {handleMouseDown}
-        onClick={resetWidth}
-         className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+          onMouseDown={handleMouseDown}
+          onClick={resetWidth}
+          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
         />
       </aside>
-      <div 
-      ref={navbarRef}
-      className={cn(
-        "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
-        isResetting && "transition-all ease-in-out duration-300",
-        isMobile && "left-0 w-full"
-      )}
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "left-0 w-full"
+        )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" onClick={resetWidth} />}
+          {isCollapsed && (
+            <MenuIcon
+              role="button"
+              className="h-6 w-6 text-muted-foreground"
+              onClick={resetWidth}
+            />
+          )}
         </nav>
       </div>
     </>
