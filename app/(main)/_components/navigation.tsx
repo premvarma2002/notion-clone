bcb6c6eea39a5aa8930106646a75651691a0eb1e
premvarma2002 <vakilverma1982@gmail.com>
2024-01-07 18:59:31 +0530

    create a note function working

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,13 +4,16 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef , useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { api } from "@/convex/_generated/api";
 
 import { cn } from "@/lib/utils";
 import { UserItem } from "./user-item";
+import { useQuery } from "convex/react";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)")
+  const documents = useQuery(api.documents.get);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -114,7 +117,9 @@ collapse();
 <UserItem />
         </div>
         <div className="mt-4">
-          <p>Documents</p>
+          {documents?.map((document) => (
+           <p key={document._id}>{document.title}</p>
+          ))}
         </div>
         <div
         onMouseDown= {handleMouseDown}
