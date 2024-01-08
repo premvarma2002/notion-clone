731661b020b95c85810a6d1bebcf8fcd8893c55f
premvarma2002 <vakilverma1982@gmail.com>
2024-01-08 21:36:27 +0530

    Note expand funtionality added with handleExpand

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
@@ -30,6 +31,14 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+      event.stopPropagation();  
+      onExpand?.();
+    }
+
    const ChevronIcon = expanded ? ChevronDown : ChevronRight
 
 
@@ -48,7 +57,7 @@ export const Item = ({
             <div
             role="button"
             className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-            onClick={() => {}}            >
+            onClick={handleExpand}            >
                 <ChevronIcon 
                 className="h-4 w-4 shrink-0 text-muted-foreground/50"
                 />
@@ -73,3 +82,17 @@ export const Item = ({
     </div>
   );
 };
+
+Item.skeleton = function ItemSkeleton({level} : {level?: number}) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${(level * 12) + 25}px`: "12px"
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+    <Skeleton className="h-4 w-4"/>
+    <Skeleton className="h-4 w-[30%]"/>
+    </div>
+  )
+}
