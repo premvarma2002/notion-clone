fd75b1e4e5397772c7443626c9ad403c0f655c19
premvarma2002 <vakilverma1982@gmail.com>
2024-01-11 23:08:43 +0530

    code formatted

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -5,7 +5,14 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus, Trash } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronRight,
+  LucideIcon,
+  MoreHorizontal,
+  Plus,
+  Trash,
+} from "lucide-react";
 import { act } from "react-dom/test-utils";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
@@ -49,17 +56,15 @@ export const Item = ({
   const create = useMutation(api.documents.create);
   const archive = useMutation(api.documents.archive);
 
-  const onArchive = (
-     event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     event.stopPropagation();
     if (!id) return;
-    const promise = archive({  id });
+    const promise = archive({ id });
 
-    toast.promise(promise,{
+    toast.promise(promise, {
       loading: "Moving to trash...",
       success: "Moved to trash...",
-      error: "Failed to archive note"
+      error: "Failed to archive note",
     });
   };
 
@@ -126,28 +131,25 @@ export const Item = ({
       {!!id && (
         <div className="ml-auto flex items-center gap-x-2">
           <DropdownMenu>
-            <DropdownMenuTrigger 
-            onClick={(e) => e.stopPropagation()}
-            asChild
-            >
-            <div
-            role="button"
-            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 "
-            >
-            <MoreHorizontal className="h-4 w-4 text-muted-foreground"/>
-            </div>
+            <DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
+              <div
+                role="button"
+                className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 "
+              >
+                <MoreHorizontal className="h-4 w-4 text-muted-foreground" />
+              </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent
-            className="w-60"
-            align="start"
-            side="right"
-            forceMount
+              className="w-60"
+              align="start"
+              side="right"
+              forceMount
             >
-             <DropdownMenuItem onClick={onArchive}>
-              <Trash className="h-4 w-4 mr-2"/>
-                 Delete
-             </DropdownMenuItem>
-             <DropdownMenuSeparator/>
+              <DropdownMenuItem onClick={onArchive}>
+                <Trash className="h-4 w-4 mr-2" />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
               <div className="text-xs text-muted-foreground p-2">
                 last edited by: {user?.fullName}
               </div>
