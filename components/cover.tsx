395766605dedc899fb74245c04246841f1b5c0ef
premvarma2002 <vakilverma1982@gmail.com>
2024-01-15 22:15:39 +0530

    Change cover and remove feature added

--- components/cover.tsx
+++ components/cover.tsx
@@ -2,6 +2,13 @@
 
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { useMutation } from "convex/react";
+import { Button } from "./ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface CoverImageProps {
     url?: string;
@@ -12,6 +19,16 @@ export const Cover = ({
     url,
     preview,
 } : CoverImageProps) => {
+   const params = useParams();
+   const coverImage = useCoverImage();
+   const removeCoverImage = useMutation(api.documents.removeCoverImage);
+
+   const onRemove = () => {
+    removeCoverImage({
+        id: params.documentId as Id<"documents">
+    });
+   }
+
   return (
     <div className={cn(
         "relative w-full h-[30vh] group",
@@ -25,6 +42,28 @@ export const Cover = ({
              alt="Cover"
              className="object-cover"
             />
+        )}
+        {url && !preview && (
+            <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+            <Button
+            onClick={coverImage.onOpen}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+            >
+                <ImageIcon className="h-4 w-4 mr-2"/>
+                Change cover
+            </Button>
+            <Button
+            onClick={onRemove}
+            className="text-muted-foreground text-xs"
+            variant="destructive"
+            size="sm"
+            >
+                <X className="h-4 w-4 mr-2"/>
+                Remove
+            </Button>
+            </div>
         )}
     </div>
   )
